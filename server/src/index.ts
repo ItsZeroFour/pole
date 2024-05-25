d75dbd8f6db19cb5140e25a46419cefe818898e5
itszerofour <itszerofourx@gmail.com>
2024-05-25 13:25:04 +0300

    fix cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,9 +14,15 @@ const app = express();
 /* CONSTANTS */
 const PORT = process.env.PORT || 5000;
 const MONGO_URI: string = process.env.MONGO_URI as string;
+const corsOptions = {
+  origin: 'http://ruspole.uer-ural.ru/login', // замените на домен вашего React-приложения
+  methods: ['GET', 'POST', 'PUT', "PATCH", 'DELETE'], // разрешенные методы
+  allowedHeaders: ['Content-Type', 'Authorization'] // разрешенные заголовки
+};
+
 /* MIDDLEWARES */
 app.use(express.json({ limit: "50mb" }));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
