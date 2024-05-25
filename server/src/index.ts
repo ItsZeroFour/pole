d09f454dd8fd5e0487a63ab227d2765a5da9bb56
itszerofour <itszerofourx@gmail.com>
2024-05-25 13:59:30 +0300

    fix cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,14 +15,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI: string = process.env.MONGO_URI as string;
 const corsOptions = {
-  origin: 'http://ruspole.uer-ural.ru/login', // замените на домен вашего React-приложения
-  methods: ['GET', 'POST', 'PUT', "PATCH", 'DELETE'], // разрешенные методы
-  allowedHeaders: ['Content-Type', 'Authorization'] // разрешенные заголовки
+  origin: 'http://ruspole.uer-ural.ru', // Замените на домен вашего React-приложения
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Разрешить отправку куки и авторизационных данных
 };
 
 /* MIDDLEWARES */
 app.use(express.json({ limit: "50mb" }));
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
