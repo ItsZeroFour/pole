75fdab832ab11f3bed06a608bf9d3e9008ae5bd9
itszerofour <itszerofourx@gmail.com>
2024-06-03 09:40:22 +0300

    add soem changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,13 +11,18 @@ import VillageRoutes from "./routes/village.js";
 dotenv.config({ path: "./.env" });
 const app = express();
 
+const corsOptions = {
+  origin: 'http://dev2.uer-ural.ru',
+  optionsSuccessStatus: 200,
+};
+
 /* CONSTANTS */
 const PORT = process.env.PORT || 5000;
 const MONGO_URI: string = process.env.MONGO_URI as string;
 
 /* MIDDLEWARES */
 app.use(express.json({ limit: "50mb" }));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
